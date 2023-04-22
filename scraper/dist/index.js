a70c550eabf57fbfce3145f92603acf84a14be22
FraSma <francemazzi@gmail.com>
2023-04-23 01:58:22 +0200

    fix: add button and refket to project wq
    wq

--- scraper/dist/index.js
+++ scraper/dist/index.js
@@ -1,15 +1,40 @@
 import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+//PORT OF SERVER
 const port = 8000;
 const app = express();
+/**
+ * ROUTING AREA
+ */
 app.get("/", (req, res) => {
     res.send("TEST 4 🧑🏻‍💻");
 });
 app.get("/hi", (req, res) => {
     res.send("CIaone 🧑🏻‍💻");
 });
-var emoji = String.fromCodePoint(0x1f9be);
+/**
+ * LISTEN AREA
+ */
 app.listen(port, () => {
+    var emoji = String.fromCodePoint(0x1f9be);
     console.log(`${emoji} IN ASCOLTO ALLA PORTA ${port}`);
     console.log("http://localhost:8000/");
+});
+//TEST AREA for scraper
+const url = "https://ec.europa.eu/eurostat/databrowser/view/tai08/default/table?lang=en";
+axios(url)
+    .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $("table-header-text", html).each(function () {
+        const titleTable = [];
+        const titleOfData = $(this).text;
+        titleTable.push({ titleOfData });
+        console.log(titleTable);
+    });
+})
+    .catch((error) => {
+    console.log("ERRORE " + error);
 });
 //# sourceMappingURL=index.js.map
--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,10 +1,17 @@
 import fs from "fs";
 import express, { Express, Request, Response } from "express";
+import axios from "axios";
+import cheerio from "cheerio";
+import { title } from "process";
 
+//PORT OF SERVER
 const port = 8000;
 
 const app: Express = express();
 
+/**
+ * ROUTING AREA
+ */
 app.get("/", (req: Request, res: Response) => {
   res.send("TEST 4 🧑🏻‍💻");
 });
@@ -13,8 +20,30 @@ app.get("/hi", (req: Request, res: Response) => {
   res.send("CIaone 🧑🏻‍💻");
 });
 
-var emoji = String.fromCodePoint(0x1f9be);
+/**
+ * LISTEN AREA
+ */
 app.listen(port, () => {
+  var emoji = String.fromCodePoint(0x1f9be);
   console.log(`${emoji} IN ASCOLTO ALLA PORTA ${port}`);
   console.log("http://localhost:8000/");
 });
+
+//TEST AREA for scraper
+const url: string =
+  "https://ec.europa.eu/eurostat/databrowser/view/tai08/default/table?lang=en";
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $("table-header-text", html).each(function () {
+      const titleTable = [];
+      const titleOfData = $(this).text;
+      titleTable.push({ titleOfData });
+      console.log(titleTable);
+    });
+  })
+  .catch((error) => {
+    console.log("ERRORE " + error);
+  });
