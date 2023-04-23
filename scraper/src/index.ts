ad483784b881c0ef1a0e4ba0774697616886fd6d
FraSma <francemazzi@gmail.com>
2023-04-24 01:16:15 +0200

    fix: browser code

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,8 +1,14 @@
 import fs from "fs";
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, response } from "express";
 import axios from "axios";
 import cheerio from "cheerio";
 import { title } from "process";
+import getUrls from "get-urls";
+import { test } from "node:test";
+import { text } from "stream/consumers";
+import { request } from "http";
+import cors from "cors";
+import puppeteer, { Browser } from "puppeteer";
 
 //PORT OF SERVER
 const port = 8000;
@@ -20,6 +26,98 @@ app.get("/hi", (req: Request, res: Response) => {
   res.send("CIaone 🧑🏻‍💻");
 });
 
+//TEST AREA for scraper
+const url: string =
+  "https://ec.europa.eu/eurostat/databrowser/view/tai08/default/table?lang=en";
+
+axios(url)
+  .then(async (response) => {
+    const html = await response.data;
+    const $ = cheerio.load(html);
+    const data = [];
+
+    $(".infobox__title", html).each((index, element) => {
+      const titleOfData = $(element).text();
+      console.log(titleOfData);
+      data.push({ titleOfData });
+    });
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("ERRORE " + error);
+  });
+
+//TEST DATA FIRESTORE
+const scrapeData = (text) => {
+  const urls = Array.from(getUrls(text));
+  const request = urls.map(async (url) => {
+    const res = await fetch(url);
+    const html = await res.text();
+    const $ = cheerio.load(html);
+    const getMetatag = (name) =>
+      $(`meta[name${name}]`).attr("content") ||
+      $(`meta[property="og:${name}"]`).attr("content") ||
+      $(`meta[property="twitter:${name}"]`).attr("content");
+
+    return {
+      url,
+      title: $("title").text(),
+      favicon: $('link[rel="shortcut icon"]').attr("href"),
+      // description: $("meta[name=description]").attr("content"),
+
+      description: getMetatag("description"),
+      image: getMetatag("image"),
+      author: getMetatag("author"),
+    };
+  });
+
+  return Promise.all(request);
+};
+
+//test puppeteer
+const main = async () => {
+  const browser: Browser = await puppeteer.launch({ headless: false });
+
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  await browser.close();
+};
+
+main();
+// app.get("/test", async (req: Request, res: Response) => {
+//   const body = JSON.parse(req.body);
+//   const data = await scrapeData(body.text);
+//   res.send("DATI - body 🧑🏻‍💻");
+//   res.send(body);
+//   res.send("DATI - data 🧑🏻‍💻");
+//   res.send(data);
+// });
+
+// async function getTextFromUrl(url: string): Promise<string[]> {
+//   try {
+//     const response = await axios.get(url);
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+
+//     const data: string[] = [];
+
+//     $(".infobox__title ux-u-color-primary").each((index, element) => {
+//       const text = $(element).text();
+//       console.log(element);
+//       data.push(text);
+//     });
+//     return data;
+//   } catch (error) {
+//     console.log("ERRORE " + error);
+//     return [];
+//   }
+// }
+
+// getTextFromUrl(url).then((data) => {
+//   console.log(`data ${data}`);
+// });
+
 /**
  * LISTEN AREA
  */
@@ -28,22 +126,3 @@ app.listen(port, () => {
   console.log(`${emoji} IN ASCOLTO ALLA PORTA ${port}`);
   console.log("http://localhost:8000/");
 });
-
-//TEST AREA for scraper
-const url: string =
-  "https://ec.europa.eu/eurostat/databrowser/view/tai08/default/table?lang=en";
-
-axios(url)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    $("table-header-text", html).each(function () {
-      const titleTable = [];
-      const titleOfData = $(this).text;
-      titleTable.push({ titleOfData });
-      console.log(titleTable);
-    });
-  })
-  .catch((error) => {
-    console.log("ERRORE " + error);
-  });
